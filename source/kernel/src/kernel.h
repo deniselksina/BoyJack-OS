19d940933aaf959cb94816d256df6d65c042422b
deniselksina <elksina40@gmail.com>
2025-02-04 07:38:22 +0300

    Create kernel.h

---
+++ source/kernel/src/kernel.h
@@ -0,0 +1,11 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+// Инициализация ядра
+void kernel_init(void);
+
+// Функции для управления памятью, процессами и т. д.
+void kernel_shutdown(void);
+void kernel_halt(void);
+
+#endif // KERNEL_H
